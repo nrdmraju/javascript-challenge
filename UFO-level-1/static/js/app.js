49ae8f1cdab817af597a0d141d6823c8aaf92307
Rahul Damaraju <rahuldamaraju@Rahuls-MacBook-Pro.local>
2020-11-13 23:33:25 -0600

    completed the app.js file and was able to filter the data on the webpage.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var table_body = d3.select("tbody");
+tableData.forEach((report) => {
+    console.log(data);
+    var a = table_body.append('tr');
+        Object.entries(report).forEach(([key, value]) => {
+            console.log(key, value);
+                var cell = a.append('td');
+                    cell.text(value);
+    });
+});
+
+// this controls the submit button
+var Submit = d3.select("#filter-btn");
+    Submit.on("click", function(){
+        d3.select("tbody").html("");
+        d3.event.preventDefault();
+
+var dateTime = d3.select("#datetime").property("value");
+    console.log(dateTime);
+        var filteredData = tableData.filter(record => record.datetime === dateTime);
+            console.log(filteredData);
+
+filteredData.forEach((report) => {
+    var b = table_body.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+            var cell = b.append('td');
+                cell.text(value);
+    });
+});
+});
